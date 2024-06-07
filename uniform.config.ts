f22657b28012c769605773a3a5e19499796767a6
Robert van den Boorn <robertb@uniform.dev>
2024-06-07 08:57:18 +0200

    added sync configuration

---
+++ uniform.config.ts
@@ -0,0 +1,30 @@
+import type { CLIConfiguration } from '@uniformdev/cli';
+
+const config: CLIConfiguration = {
+  serialization: {
+    entitiesConfig: {
+      aggregate: {},
+      asset: {},
+      category: {},
+      composition: { publish: true },
+      contentType: {},
+      component: {},
+      dataType: {},
+      enrichment: {},
+      entry: { publish: true },
+      locale: {},
+      pattern: { publish: true },
+      projectMapDefinition: {},
+      projectMapNode: {},
+      quirk: {},
+      redirect: {},
+      signal: {},
+      test: {},
+    },
+    directory: './content',
+    format: 'yaml',
+    mode: 'mirror'
+  },
+};
+
+module.exports = config;
